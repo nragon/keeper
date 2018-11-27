cfa969d2af8a02707b521d6fe0db4798abb9575f
nragon <Nunoo_7@hotmail.com>
2018-11-27 17:33:11 +0000

    Add files via upload

---
+++ bin/keeper.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o posix
+
+if [[ -z "$KEEPER_HOME" ]]; then
+  export KEEPER_HOME="$(cd "`dirname "$0"`"/..; pwd)"
+fi
+
+finish() {
+  pkill -9 -P $$
+}
+
+trap finish EXIT
+
+if [[ ! -e "${KEEPER_HOME}/log/keeper.log" ]]; then
+    mkdir "${KEEPER_HOME}/log"
+    touch "${KEEPER_HOME}/log/keeper.log"
+fi
+
+sudo -E "${KEEPER_HOME}/bin/python3" -u "${KEEPER_HOME}/keeper.py" >> "${KEEPER_HOME}/log/keeper.log" 2>&1
+wait $!
